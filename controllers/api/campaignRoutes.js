bc13ca5a6a984bf09f75f8c51efa81e2e119d2c7
Bryan Peens <peensbryan1@gmail.com>
2024-04-27 16:37:00 -0400

    campaign fix

--- controllers/api/campaignRoutes.js
+++ controllers/api/campaignRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { Campaign } = require('../../models');
-
 router.get('/', async (req, res) => {
   try {
     const campaignData = await Campaign.findAll({
@@ -11,15 +10,12 @@ router.get('/', async (req, res) => {
         },
       ],
     });
-
     const campaigns = campaignData.map(campaign => campaign.get({ plain: true }));
-
-    res.render('campaign', { 
+    res.render('campaign', {
       campaigns
      });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 module.exports = router;
