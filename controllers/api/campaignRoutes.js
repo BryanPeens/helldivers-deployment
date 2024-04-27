8845ea93cf85f19f4818b072624a2bde509c2de2
Ze <elawrence.bda@gmail.com>
2024-04-27 14:26:05 -0400

    somewhat functional server browser added

--- controllers/api/campaignRoutes.js
+++ controllers/api/campaignRoutes.js
@@ -22,4 +22,17 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const campaignData = await Campaign.create(
+      {
+        campaign_name: req.body.name,
+      },
+    );
+    res.status(200).json(campaignData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
