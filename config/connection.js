1ea6ee312f390eba521589d01ea897f63702a34c
Edward <42353819+ZeDevGit@users.noreply.github.com>
2024-04-29 19:39:32 -0400

    Update connection.js
    Switched host to IP

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ if (process.env.DB_URL) {
     // process.env.DB_HOST,
     // process.env.DB_PORT,
     {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'postgres'
     }
   );
